5f50c740a009e3f0bcba39b1c15ea89122db2405
i.nekrasov <nekrulyaa@mail.ru>
2023-10-19 13:55:23 +0500

    new carousel

--- src/pages/Carous.jsx
+++ src/pages/Carous.jsx
@@ -1,59 +1,105 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
+import React, { useEffect, useState } from "react";
 
-export default class AutoPlayMethods extends Component {
-  constructor(props) {
-    super(props);
-    this.play = this.play.bind(this);
-    this.pause = this.pause.bind(this);
-  }
-  play() {
-    this.slider.slickPlay();
-  }
-  pause() {
-    this.slider.slickPause();
-  }
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 1200,
-      autoplaySpeed: 100,
-      cssEase: "ease",
-    };
+const Carous = () => {
+  const [animationMove, setAnimationMove] = useState(false);
+  const radius = 300;
+  const [anim, setAnim] = useState();
 
-    return (
-      <div className="carouselLayout">
-        <h2 className="carousTitle">Auto Play</h2>
-        <Slider ref={(slider) => (this.slider = slider)} {...settings}>
-          <div className="carousItem">
-            <img src="iphone1.jpg" alt="" />
-          </div>
-          <div className="carousItem">
-            <img src="iphone2.jpg" alt="" />
-          </div>
-          <div className="carousItem">
-            <img src="iphone3.jpg" alt="" />
-          </div>
-          <div className="carousItem">
-            <img src="iphone4.jpg" alt="" />
-          </div>
-          <div className="carousItem">
-            <img src="iphone5.jpg" alt="" />
-          </div>
-        </Slider>
-        <div className="carouselButtons">
-          <button className="carouselButton" onClick={this.play}>
-            Play
-          </button>
-          <button className="carouselButton" onClick={this.pause}>
-            Pause
-          </button>
+  // function sleep(ms) {
+  //   return new Promise((resolve) => setTimeout(resolve, ms));
+  // }
+
+  const [images, setImages] = useState([
+    { src: "ecommerce.png", alt: "ecommerce", angle: 0, x: 0, y: 0 },
+    { src: "speaker.png", alt: "speaker", angle: 0, x: 0, y: 0 },
+    {
+      src: "social-media-marketing.png",
+      alt: "social-media-marketing",
+      angle: 0,
+      x: 0,
+      y: 0,
+    },
+    { src: "idea.png", alt: "idea", angle: 0, x: 0, y: 0 },
+    { src: "advertising.png", alt: "advertising", angle: 0, x: 0, y: 0 },
+    { src: "bar-chart.png", alt: "bar-chart", angle: 0, x: 0, y: 0 },
+  ]);
+
+  function positioning() {
+    for (let i in images) {
+      let temp = [...images];
+      temp[i].x =
+        Math.cos((((i * 360) / images.length) * Math.PI) / 180) * radius;
+      temp[i].y =
+        Math.sin((((i * 360) / images.length) * Math.PI) / 180) * radius;
+      temp[i].angle = (i * 360) / temp.length;
+      setImages([...temp]);
+    }
+  }
+
+  useEffect(() => {
+    positioning();
+  }, []);
+
+  function changeAnim(animationMove) {
+    if (animationMove) {
+      setAnim(
+        setInterval(() => {
+          let temp = [...images];
+          for (let k = 0; k < images.length; k++) {
+            temp[k].angle += 0.5;
+            if (temp[k].angle >= 360) {
+              temp[k].angle = 0;
+            }
+            temp[k].x = Math.cos((temp[k].angle * Math.PI) / 180) * radius;
+            temp[k].y = Math.sin((temp[k].angle * Math.PI) / 180) * radius;
+          }
+          setImages([...temp]);
+        }, 20)
+      );
+    } else {
+      clearInterval(anim);
+    }
+  }
+
+  useEffect(() => {
+    changeAnim(animationMove);
+  }, [animationMove]);
+
+  return (
+    <div className="carouselLayout">
+      <div className="content carousel__content">
+        <button
+          onClick={async () => {
+            setAnimationMove(true);
+          }}
+        >
+          Запустить карусель
+        </button>
+
+        <div className="logos">
+          {images.map((image) => {
+            return (
+              <img
+                className="logos__img"
+                src={image.src}
+                alt={image.alt}
+                key={image.src}
+                style={{ top: image.y + "px", left: image.x + "px" }}
+              />
+            );
+          })}
         </div>
+        <button
+          onClick={async () => {
+            setAnimationMove(false);
+            console.log("false нажат");
+          }}
+        >
+          Остановить карусель
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Carous;
